0f86dc4b3fe7b6c32672c7076ba04f4bdd12bc88
Doug McGeehan <djmvfb@mst.edu>
2012-03-02 23:42:37 -0600

    First line of sample input working, but second line is not

--- 4934/src/ropes.c
+++ 4934/src/ropes.c
@@ -3,31 +3,66 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+#define NUM_ROPES 3
 
 int main()
 {
   int numberOfPitches;
   int* pitchHeight;
 
-  const int ROPES[3] = {50, 60, 70};
+  const int ROPES[NUM_ROPES] = {50, 60, 70};
 
   scanf("%d", &numberOfPitches);
   while (numberOfPitches != 0)
   { 
-    printf("%s %d\n", "Number of pitches: ", numberOfPitches);
-
     // Load the pitch heights into an array
     pitchHeight = (int *) malloc(numberOfPitches * sizeof (int));
     for (int i=0; i < numberOfPitches; i++)
     {
-      printf("%s %d%s", "Pitch number", i, ": ");
       scanf("%d", &(pitchHeight[i]));
-      printf("%d\n", pitchHeight[i]);
+    }
+
+    for (int i=0; i < NUM_ROPES; i++)
+    {
+      for (int pitch=0; pitch < numberOfPitches; pitch++)
+      {
+        const int PITCH_HEIGHT = pitchHeight[pitch];
+        const int ROPE_LENGTH  = ROPES[i];
+        bool notEnoughRope = (PITCH_HEIGHT > ROPE_LENGTH);
+        if (notEnoughRope)
+        {
+          printf("%d ", 0);
+        }
+        else
+        {
+          int lengthOnTop, lengthInUse, lengthOnBottom;
+          int climbers = 1;
+
+          lengthOnTop = 0;
+          lengthInUse = PITCH_HEIGHT;
+          lengthOnBottom = ROPE_LENGTH - lengthInUse;
+          while (lengthOnBottom >= 0)
+          {
+            lengthOnBottom -= PITCH_HEIGHT;
+            climbers++;
+          }
+
+          if (lengthOnBottom >= 0)
+          {
+            climbers++;
+          }
+
+          printf("%d ", climbers);
+        }
+      }
     }
 
     free(pitchHeight);
     pitchHeight = NULL;
 
+    printf("\n");
     //printf("%d %d %d\n",mOriginal,nOriginal,max);
     scanf("%d", &numberOfPitches);
   }
