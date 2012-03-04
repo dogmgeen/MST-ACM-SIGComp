b608911452e268d74584e6e05b99ad9e56c5c129
Doug McGeehan <djmvfb@mst.edu>
2012-03-03 21:21:02 -0600

    Functioning again

--- 4934/src/climb.h
+++
@@ -1,6 +0,0 @@
-
-int* loadPitchHeights(const int);
-int totalHeightOfAllPitches(const int*, const int);
-int getHighestPitch(const int*, const int);
-int getNumberOfClimbersForGivenRope(const int, const int*, const int);
-
