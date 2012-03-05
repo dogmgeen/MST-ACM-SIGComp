35a5f256370a1c7be160c32e0f5b529af08315ea
Doug McGeehan <djmvfb@mst.edu>
2012-03-05 00:25:49 -0600

    baseline_solution.c would go into an infinite loop, it appears. Whoops.

--- sample/baseline_solution.c
+++ sample/baseline_solution.c
@@ -3,14 +3,16 @@ Author: Doug McGeehan (djmvfb@mst.edu)
 Date: March 4th, 2012
 */
 
+#define BUFFER_SIZE 1024
 #include <stdio.h>
 
 int main()
 {
-  while (!feof(stdin))
+  char buffer[BUFFER_SIZE];
+
+  while (fgets(buffer, BUFFER_SIZE, stdin))
   {
-    puts("Just keep looping until the EOF");
-    scanf("%*[ 0123456789]\n");
+    puts(buffer);
   }
 
   return 0;
