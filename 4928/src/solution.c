620eb5f495292dfd21cbdcc96bc619ec856b3ef6
Doug McGeehan <djmvfb@mst.edu>
2012-03-05 01:15:42 -0600

    Not working as I hoped it would. The int a the beginning of the line is continuously being output when iterating over the books.

--- 4928/src/solution.c
+++ 4928/src/solution.c
@@ -21,11 +21,17 @@ int main()
     fgets(buffer, BUFFER_SIZE, stdin);
 
     int numberOfBooks;
-    sscanf(buffer, "%d", &numberOfBooks);
+    sscanf(buffer, "%d ", &numberOfBooks);
+
+printf("  Number of books for series #%d: %d\n", series_itr+1, numberOfBooks);
 
     int book_itr;
     for (book_itr=0; book_itr<numberOfBooks; book_itr++)
     {
+      int durationOfMovie;
+      sscanf(buffer, "%d ", &durationOfMovie);
+
+printf("    Length of the movie for book #%d: %d\n", book_itr+1, durationOfMovie);
     }
 
     printf(TEMPLATE_LINE, 0);
